e559ed555f0f1b9c47ff2400c5e8fef3810d7872
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-05-21 15:41:37 +0500

    Add real Firebase config

--- js/cfg.js
+++ js/cfg.js
@@ -1,3 +1,3 @@
-const FIREBASE_CONFIG={apiKey:"AIzaSyBXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",authDomain:"business-dogs-game.firebaseapp.com",databaseURL:"https://business-dogs-game-default-rtdb.firebaseio.com",projectId:"business-dogs-game",storageBucket:"business-dogs-game.appspot.com",messagingSenderId:"XXXXXXXXXXXX",appId:"1:XXXXXXXXXXXX:web:XXXXXXXXXXXXXXXX"};
+const FIREBASE_CONFIG={apiKey:"AIzaSyBo-UlMUM-u7SrB0LS0214iQahISipJ-Eo",authDomain:"business-dogs-game.firebaseapp.com",databaseURL:"https://business-dogs-game-default-rtdb.firebaseio.com",projectId:"business-dogs-game",storageBucket:"business-dogs-game.firebasestorage.app",messagingSenderId:"944829622013",appId:"1:944829622013:web:f56e7d89b51bac0b8fc094"};
 if(!firebase.apps.length)firebase.initializeApp(FIREBASE_CONFIG);
 const DB=firebase.database();
